5251bb14aa360fcfbfa56195a4c123dd02f1c389
MuhammedCubukcu <muhammedcubukcu0@gmail.com>
2024-02-26 07:53:55 +0300

    bugs fixed and created model.pkl file

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import os
 import numpy as np
 from skimage.io import imread
 from skimage.transform import resize
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score,confusion_matrix
+import pickle
 
 
 
@@ -21,46 +21,47 @@ with open(input_txt, 'r') as f:
         value = value.strip('\n')
         labels.append(value)
 
-print(labels)
-
-for category in labels:
-    category_dir = os.path.join(input_dir, category)
-    for file_name in os.listdir(category_dir):
-        file_path = os.path.join(category_dir, file_name)
-        img = imread(file_path)
+categories = []
+for index, category in enumerate(labels):
+    for file in os.listdir(os.path.join(input_dir,category)):
+        img_path = os.path.join(input_dir,category,file)
+        img = imread(img_path)
         img = resize(img, (15, 15))
-        images.append(img)
+        images.append(img.flatten())
+        categories.append(index)
 
 
+images = np.asarray(images)
+categories = np.asarray(categories)
 
 
-# Label Encoding
-le = LabelEncoder()
-labels_encoded = le.fit_transform(labels)
-
-# Convert lists to numpy arrays
-images = np.array(images)
-labels_encoded = np.array(labels_encoded)
 
 # Split Data
-X_train, X_test, y_train, y_test = train_test_split(images, labels_encoded, test_size=0.33, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(images, categories, test_size=0.33, shuffle=True, random_state=0)
 
 # Model
-classifier = SVC(probability=True)
-param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [0.0001, 0.001, 0.1, 1], 'kernel': ['rbf', 'poly']}
+classifier = SVC()
+param_grid = [{'C': [0.01, 0.1, 1, 10, 100], 'gamma': [0.001, 0.01, 0.1, 1]}]
 model = GridSearchCV(classifier, param_grid)
 model.fit(X_train,y_train)
 
 
 # Prediction
 y_pred = model.predict(X_test)
+if y_pred[0] == 0:
+    print("This a cat")
+else:
+    print("This a dog")
+
 
 # Accuracy Score
-acc = accuracy_score(y_test,y_pred)
-print(acc)
+acc = accuracy_score(y_test, y_pred)
+print("Accuracy Score {0}".format(acc))
 
 
 # Confusion Matrix
-
 matrix = confusion_matrix(y_test, y_pred)
 print(matrix)
+
+with open('model.pkl', 'wb') as file:
+    pickle.dump(model, file)
